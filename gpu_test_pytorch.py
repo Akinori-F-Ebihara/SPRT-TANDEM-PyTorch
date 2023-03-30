ba1fd8f15665157d69bf3dcc79e5a159ff7432fe
Akinori-F-Ebihara <aebihara@icloud.com>
2023-03-30 17:40:26 +0900

    Update gpu_test_pytorch.py

--- gpu_test_pytorch.py
+++ gpu_test_pytorch.py
@@ -3,12 +3,3 @@ import torch
 print(f'{torch.__version__=}')
 print(f'{torch.cuda.device_count()=}')
 print(f'{torch.cuda.is_available()=}')
-# def grab_gpu(gpu=0):
-#     # restrict visible device(s)
-#     # os.environ['CUDA_VISIBLE_DEVICES'] = str(gpu)
-#     # Get cpu or gpu device for training.
-#     device = "cuda" if torch.cuda.is_available() else "cpu"
-#     print(f'Using {device=}')
-#     return torch.device(device)
-
-# device = grab_gpu() 
